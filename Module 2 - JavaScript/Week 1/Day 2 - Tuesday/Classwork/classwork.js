c13a403fad36cb7101c1daf7c2e40dc056805580
justinluce <luce@midlandu.com>
2023-09-12 17:28:10 -0500

    adds day 2

---
+++ Module 2 - JavaScript/Week 1/Day 2 - Tuesday/Classwork/classwork.js
@@ -0,0 +1,46 @@
+//! LOOP PRACTICE
+// -----------------------------------------------------------------
+
+//! Take an input from the user. Starting with the 4th character,
+//! log all characters until the end of the input.
+
+// -----------------------------------------------------------------
+//! If the input is shorter than 4 characters, nothing should be logged.
+
+// -----------------------------------------------------------------
+//! Pick a random number. Prompt the user to guess a number. If the number is correct,
+//! end the loop and tell the user how many tries it took to guess.
+//! If it is incorrect, continue the loop.
+//! (Test functionality by logging the number that was randomly generated in the prompt).
+
+// -----------------------------------------------------------------
+//! Using nested for loops, create and log the following pattern:
+/*
+//!    *
+//!    **
+//!    ***
+//!    ****
+//!    *****
+*/
+
+// -----------------------------------------------------------------
+//! Simulate a coin flip. Start a counter at 0. If the initial flip was heads,
+//! leave the counter at 0 and log: "It took 0 retries to get heads!".
+//! If the coin was tails, try again and keep doing so until heads happens. Log the amount of retries it took.
+
+// COMPARISON PRACTICE
+// -----------------------------------------------------------------
+//! Build a simple site that prompts a user for their first name and then a number
+//! between one and one-hundred (inclusive).
+//!  - Tell them whether their number is odd or even and if its greater than,
+//!    or less than/equal to 50 and then log those messages separately to the console.
+//!  - Log every number before theirs and every number from 100 counting down to theirs in two separate loops.
+//!  - If their name is your name send an alert saying that it is a great name
+//!  - Log their name in reverse to the console.
+
+// -----------------------------------------------------------------
+//! Take the following arrays: `[-1,-2,2,10,7,8]` and `[4,-2,2,7,9,5]`
+//! and see how many items the two arrays share in common.
+//! Do this without prototypical functions (ex. array.filter and array.includes).
+//! Hint: use a nested loop
+
